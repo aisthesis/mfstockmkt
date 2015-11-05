5dc1163bc23fd8f6ceb6ecf1970aad8ecbbb0b85
Marshall Farrier <info@marshallfarrier.com>
2015-11-04 20:58:47 -0800

    cosmetic improvement

--- velocity/velo.py
+++ velocity/velo.py
@@ -14,6 +14,39 @@ import pynance as pn
 
 from mov_ext import MovingExtremumFinder
 
+def get_all(eqdata, window=100, selection='Adj Close'):
+    """
+    Return a dataframe with prices and both upward and downward 
+    velocity data.
+
+    Parameters
+    ----------
+    eqdata : DataFrame
+        Source data
+
+    window : int, optional
+        The window of prior sessions over which
+        velocity is calculated. Defaults to 100.
+
+    selection : str, optional
+        Column of `eqdata` in which historical prices
+        are specified. Defaults to 'Adj Close'.
+        
+    Returns
+    -------
+    all_vel : DataFrame
+    """
+    up_vel = up(eqdata, window, selection)
+    down_vel = down(eqdata, window, selection)
+    up_col = 'Up Vel'
+    down_col = 'Down Vel'
+    all_vel = pd.DataFrame(index=up_vel.index, columns=[selection, up_col, down_col],
+            dtype='float64')
+    all_vel.loc[:, selection] = eqdata.loc[:, selection].values[window:]
+    all_vel.loc[:, up_col] = up_vel.values
+    all_vel.loc[:, down_col] = down_vel.values
+    return all_vel
+
 def up(eqdata, window=100, selection='Adj Close'):
     """
     Return a dataframe with upward velocity data.
@@ -64,7 +97,7 @@ def _velocities(eqdata, window, selection, compfn):
     outcol = 'Velocity'
     outix = eqdata.index[window:]
     inputdata = eqdata.loc[:, selection].values
-    vels = np.empty(outix.shape[0], dtype='int64')
+    vels = np.empty_like(outix, dtype='int64')
     ext_finder = MovingExtremumFinder(inputdata, window, compfn)
     # values up to `window` don't have adequate history
     for i in range(window):
