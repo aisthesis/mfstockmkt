0d518d2469e2c9425afd728686b6bdda332f5de6
Marshall Farrier <info@marshallfarrier.com>
2015-11-05 20:44:07 -0800

    Use float between 0. and 1. regardless of window.

---
+++ velocity/constants.py
@@ -0,0 +1,10 @@
+"""
+.. Copyright (c) 2015 Marshall Farrier
+   license http://opensource.org/licenses/MIT
+
+Constants
+=========
+"""
+
+UPVEL_COL = 'Up Vel'
+DOWNVEL_COL = 'Down Vel'
--- velocity/velo.py
+++ velocity/velo.py
@@ -12,9 +12,10 @@ import numpy as np
 import pandas as pd
 import pynance as pn
 
+import constants
 from mov_ext import MovingExtremumFinder
 
-def get_all(eqdata, window=100, selection='Adj Close'):
+def rolling_vel(eqdata, window=100, selection='Adj Close'):
     """
     Return a dataframe with prices and both upward and downward 
     velocity data.
@@ -34,74 +35,26 @@ def get_all(eqdata, window=100, selection='Adj Close'):
         
     Returns
     -------
-    all_vel : DataFrame
+    mov_vel : DataFrame
     """
-    up_vel = up(eqdata, window, selection)
-    down_vel = down(eqdata, window, selection)
-    up_col = 'Up Vel'
-    down_col = 'Down Vel'
-    all_vel = pd.DataFrame(index=up_vel.index, columns=[selection, up_col, down_col],
-            dtype='float64')
-    all_vel.loc[:, selection] = eqdata.loc[:, selection].values[window:]
-    all_vel.loc[:, up_col] = up_vel.values
-    all_vel.loc[:, down_col] = down_vel.values
-    return all_vel
-
-def up(eqdata, window=100, selection='Adj Close'):
-    """
-    Return a dataframe with upward velocity data.
-
-    Parameters
-    ----------
-    eqdata : DataFrame
-        Source data
-
-    window : int, optional
-        The window of prior sessions over which
-        velocity is calculated. Defaults to 100.
-
-    selection : str, optional
-        Column of `eqdata` in which historical prices
-        are specified. Defaults to 'Adj Close'.
-        
-    Returns
-    -------
-    upvel : DataFrame
-    """
-    return _velocities(eqdata, window, selection, operator.gt)
-
-def down(eqdata, window=100, selection='Adj Close'):
-    """
-    Return a dataframe with downward velocity data.
-
-    Parameters
-    ----------
-    eqdata : DataFrame
-        Source data
-
-    window : int, optional
-        The window of prior sessions over which
-        velocity is calculated. Defaults to 100.
-
-    selection : str, optional
-        Column of `eqdata` in which historical prices
-        are specified. Defaults to 'Adj Close'.
-        
-    Returns
-    -------
-    downvel : DataFrame
-    """
-    return _velocities(eqdata, window, selection, operator.lt)
-
-def _velocities(eqdata, window, selection, compfn):
-    outcol = 'Velocity'
     outix = eqdata.index[window:]
+    upcol = constants.UPVEL_COL
+    downcol = constants.DOWNVEL_COL
+    mov_vel = pd.DataFrame(index=eqdata.index[window:], 
+            columns=[selection, upcol, downcol], dtype='float64')
+    mov_vel.loc[:, selection] = eqdata.loc[:, selection].values[window:]
+    mov_vel.loc[:, upcol] = _velocity(eqdata, window, selection, operator.gt, upcol)
+    mov_vel.loc[:, downcol] = _velocity(eqdata, window, selection, operator.lt, downcol)
+    return mov_vel
+
+def _velocity(eqdata, window, selection, compfn, outputcol):
     inputdata = eqdata.loc[:, selection].values
-    vels = np.empty_like(outix, dtype='int64')
+    vels = np.empty_like(eqdata.index[window:], dtype='float64')
     ext_finder = MovingExtremumFinder(inputdata, window, compfn)
+    win_float = float(window)
     # values up to `window` don't have adequate history
     for i in range(window):
         ext_finder.insert(i)
     for i in range(window, inputdata.shape[0]):
-        vels[i - window] = window - ext_finder.insert(i)
-    return pd.DataFrame(data=vels, index=outix, columns=[outcol], dtype='int64')
+        vels[i - window] = float(window - ext_finder.insert(i)) / win_float
+    return vels
