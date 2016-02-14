cbf2db9143a8580992ce20be71512a3187396983
Marshall Farrier <info@marshallfarrier.com>
2016-02-14 00:29:03 -0800

    Save specific options to tracking collection.

--- options/db/track.py
+++ options/db/track.py
@@ -5,6 +5,12 @@
 Interactively start and stop tracking an option.
 """
 
+import datetime as dt
+from functools import partial
+import logging
+
+import pytz
+
 class Menu(object):
 
     def __init__(self):
@@ -14,6 +20,7 @@ class Menu(object):
                     '1': self.track_single
                     }
                 }
+
     def exec_menu(self, name, choice):
         try:
             self.actions[name][choice.strip()]()
@@ -24,8 +31,8 @@ class Menu(object):
     def start(self):
         print('Main menu')
         print('Select action:')
-        print('1. Track single option')
-        print('2. Track spread')
+        print('1. Start tracking single option')
+        print('2. Start tracking spread')
         print('3. Stop tracking single option')
         print('4. Stop tracking spread')
         print('\n0. Quit')
@@ -33,11 +40,37 @@ class Menu(object):
         self.exec_menu('main', choice)
         
     def track_single(self):
-        underlying = input('Underlying equity: ').upper()
-        opttype = input('Option type (call or put): ').lower()
-        strike = float(input('Strike: '))
-        expiry = input('Expiration: ')
-        print(underlying, opttype, strike, expiry)
+        entry = {}
+        entry['Underlying'] = input('Underlying equity: ').upper()
+        entry['Opt_Type'] = input('Option type (call or put): ').lower()
+        entry['Strike'] = float(input('Strike: '))
+        entry['Expiry'] = _getexpdt(input('Expiration (yyyy-mm-dd): '))
+        _saveentries((entry,), None, None)
+
+def _getexpdt(expstr):
+    eastern = pytz.timezone('US/Eastern')
+    return eastern.localize(dt.datetime.strptime(expstr, '%Y-%m-%d')).replace(hour=23,
+            minute=59, second=59)
+
+def _saveentries(entries, logger, client):
+    print('\nSaving the following options:')
+    _showentries(entries)
+    choice = input('\nOK to proceed (y/n)? ').lower()
+    if choice == 'y':
+        print('proceeding')
+    else:
+        print('Aborting: options not saved!')
+
+def _showentries(entries):
+    for entry in entries:
+        print('')
+        _showentry(entry)
+
+def _showentry(entry):
+    print('Underlying: {}'.format(entry['Underlying']))
+    print('Opt_Type: {}'.format(entry['Opt_Type']))
+    print('Strike: {:.2f}'.format(entry['Strike']))
+    print('Expiry: {}'.format(entry['Expiry'].strftime('%Y-%m-%d')))
 
 if __name__ == '__main__':
     Menu().start()
